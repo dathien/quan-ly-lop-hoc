90727f7aeff82873e93f95fd88d85d098c18f92b
dathien <47940133+dathien@users.noreply.github.com>
2026-09-12 13:54:17 +0700

    Update me.js

--- api/auth/me.js
+++ api/auth/me.js
@@ -1,1 +1,7 @@
+import { getSession, publicUser, json } from '../_auth.js';
+export default async function handler(req,res){
+  if(req.method!=='GET') return json(res,405,{ok:false,error:'Method not allowed'});
+  const user=await getSession(req);
+  return json(res,200,{ok:true,authenticated:!!user,user:publicUser(user)});
+}
 
